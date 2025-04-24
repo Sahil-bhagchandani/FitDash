3305d6e260e6ecaa07fd05af18c22b2c6e2732f7
Sahil B <sahilbhagchandani04@gmail.com>
2025-04-24 15:14:04 +0530

    added important files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),
-    react()],
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'dist',  // Ensure the build output is placed here
+  },
 })
+
